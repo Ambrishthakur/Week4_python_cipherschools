1614748fac2408ed71ffea35cb913093b708d142
Ambrishthakur <114800245+Ambrishthakur@users.noreply.github.com>
2023-01-05 23:50:32 +0530

    Add files via upload

---
+++ builtin_errors.py
@@ -0,0 +1,38 @@
+# chapter__17
+# built in errors 
+# exception , how to handle them
+# raise your own errors , debugging , etc.
+
+ 
+# 1. syntax error - when syntax is not proper
+
+
+# def func:
+#     pass
+# name = 'harshit'*
+
+
+
+# 2. indentation errors -  when spaces are not proper
+
+
+# def func: -  (spaces)
+#    print("hello")
+#  print("world")
+
+
+# 3. name error - when function is not defined
+
+#print(name)
+
+
+# 4. value error
+# s = '123'
+# print(int(s))
+
+
+
+
+
+
+
